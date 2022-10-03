00393961433a783b56fd56927d7eb0dfa3be62aa
Ginger Owen <gingerowen0421@gmail.com>
2022-10-02 21:29:00 -0600

    worked on notes

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,10 +3,7 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    // Store the triggered events
     window.deferredPrompt = event;
-
-    // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
 });
 
@@ -21,7 +18,7 @@ butInstall.addEventListener('click', async () => {
   // Show prompt
   promptEvent.prompt();
   
-  // Reset the deferred prompt variable, it can only be used once.
+  // reset variable
   window.deferredPrompt = null;
   
   butInstall.classList.toggle('hidden', true);
@@ -29,6 +26,6 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-      // Clear prompt
+      // set prompt to null to clear
   window.deferredPrompt = null;
 });
